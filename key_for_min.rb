9c8c6337e2a66233c5054b09b83167df0140e861
Jina Strickland <jina30@gmail.com>
2020-07-03 20:49:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  max_num = name_hash[:key =>]
-  name_hash.each do | key, value |
-
+  max_num = name_hash[key][0]
+    name_hash.each do | key, value |
+      
 
   return name_hash[:key]
 end
