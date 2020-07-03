51943a81b09ced23cf5930e8ca8ece1d6a00f6ae
Jina Strickland <jina30@gmail.com>
2020-07-03 22:08:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,31 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+# def key_for_min_value(name_hash)
+#
+#     name_hash.each_value do | k, v |
+#       high_num = name_hash[k][0]
+#       if high_num < name_hash[k][v]
+#         high_num = name_hash[k][v]
+#         return name_hash[k]
+#       end
+#     end
+# end
 
-    name_hash.each_value do | k, v |
-      high_num = name_hash[k][0]
-      if high_num < name_hash[k][v]
-        high_num = name_hash[k][v]
-        return name_hash[k]
-      end
+
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
     end
+  end
+  lowest_key
 end
+
 
 #
 # def select_winner(passengers)
