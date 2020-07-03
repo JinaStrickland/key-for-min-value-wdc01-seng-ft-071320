76bb0f4fdeaef2b5ce7b6f03952a80b09a02b935
Jina Strickland <jina30@gmail.com>
2020-07-03 20:23:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,3 +16,21 @@ def find_valid_calls(planeteer_calls)
 end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
+
+  def find_min_in_nested_arrays(src)
+  low_temp_src = []
+  row_index = 0
+    while row_index < src.count do
+      element_index = 0
+      max_value = src[row_index][0]
+    while element_index < src[row_index].length do
+      if src[row_index][element_index] < max_value
+        max_value = src[row_index][element_index]
+      end
+        element_index += 1
+    end
+      low_temp_src << max_value
+      row_index += 1
+    end
+      low_temp_src
+  end
