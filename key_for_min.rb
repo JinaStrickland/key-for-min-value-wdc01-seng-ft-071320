18b4a37a3eb20bae08c053327fc7dad73550b890
Jina Strickland <jina30@gmail.com>
2020-07-03 22:11:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,8 +16,8 @@
 
 def key_for_min_value(name_hash)
   lowest_k = nil
-  lowest_v = nil
-  name_hash.each do |k, v|
+  lowest_v = 0
+  name_hash.each_value do |k, v|
     if v < lowest_v
       lowest_v = v
       lowest_k = k
